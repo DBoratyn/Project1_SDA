29d98be1beed7ce87a329eee930a3666390785bb
Dagmara Boratyn <daggi.bor97@gmail.com>
2023-02-12 13:54:48 +0100

    Added sprint feature

--- Project2D_SDA/Assets/Source/Player/PlayerMovement.cs
+++ Project2D_SDA/Assets/Source/Player/PlayerMovement.cs
@@ -9,30 +9,61 @@ namespace Player
 {
     public class PlayerMovement : MonoBehaviour
     {
+        private const string HORIZONTAL_AXIS = "Horizontal";
+        private const string SPRINT_AXIS = "Sprint";
+
         [SerializeField]
         private Rigidbody2D rigidbody2D;
 
-        public void Init()
-        {
-            //ustaw startow? pozycj?
+        [SerializeField]
+        private float playerSpeed;
+
+        [SerializeField]
+        private float sprintMultiplayer;
+
+        [SerializeField]
+        private float maxWalkSpeed;
+
+        [SerializeField]
+        private float maxRunSpeed;
+
+        public void Init() { 
             gameObject.SetActive(true);
         }
 
         public void UpdatePosition()
         {
-            if (Input.GetKey(KeyCode.A))
+            float playerInput = Input.GetAxisRaw(HORIZONTAL_AXIS);
+            float sprintInput = Input.GetAxisRaw(SPRINT_AXIS);
+
+            if (playerInput == 0)
             {
-                if (rigidbody2D.velocity.x > 0)
-                    rigidbody2D.velocity = Vector2.zero;
-                rigidbody2D.AddForce(Vector2.left * 10, ForceMode2D.Force);
+                rigidbody2D.velocity = Vector2.zero;
             }
 
-            if (Input.GetKey(KeyCode.D))
+            float finalMultiplier = playerInput * playerSpeed;
+            if (sprintInput > 0)
             {
-                if (rigidbody2D.velocity.x < 0)
-                    rigidbody2D.velocity = Vector2.zero;
-                rigidbody2D.AddForce(Vector2.right * 10, ForceMode2D.Force);
+                finalMultiplier *= sprintMultiplayer;
             }
+
+            rigidbody2D.AddForce(Vector2.right * finalMultiplier, ForceMode2D.Force);
+
+            var x = rigidbody2D.velocity.x;
+            var direction = Mathf.Sign(x);
+            x = Mathf.Abs(x);
+
+            if (sprintInput == 0)
+            {
+                x = Mathf.Clamp(x, 0, maxWalkSpeed);
+            }
+            else
+            {
+                x = Mathf.Clamp(x, 0, maxRunSpeed);
+            }
+
+            Vector2 finalVelocity = new Vector2(x * direction, rigidbody2D.velocity.y);
+            rigidbody2D.velocity = finalVelocity;
         }
 
         public void Dispose()
@@ -40,5 +71,4 @@ namespace Player
             gameObject.SetActive(false);
         }
     }
-
 }
