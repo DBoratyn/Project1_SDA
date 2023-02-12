55ad48d1491eb62d7d671657aa90cf14058bea4d
Dagmara Boratyn <daggi.bor97@gmail.com>
2023-02-12 08:58:17 +0100

    Added base menu

--- Project2D_SDA/Assets/Source/Core/StateMachine/States/GameState.cs
+++ Project2D_SDA/Assets/Source/Core/StateMachine/States/GameState.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assets.Source.Core.StateMachine.States
+namespace Core
 {
     public class GameState : BaseState
     {
---
+++ Project2D_SDA/Assets/Source/UI/Views/BaseView.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    public abstract class BaseView : MonoBehaviour
+    {       
+        public virtual void ShowView() {
+            this.gameObject.SetActive(true);
+        }
+
+        public virtual void HideView() {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
